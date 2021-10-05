00bd69c26ad04882ca2f5099a7dc220e2c2e6faa
whitewolf8383 <kandt.vandf@gmail.com>
2021-10-05 15:16:11 -0600

    Movie component set

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
+import { getMovies } from "../services/fakeMovieService";
 
 class Movie extends React.Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies});
+  }
+
   render() { 
+    if (this.state.movies.length === 0)
+      return <p>There are no movies in the database.</p>
     return (
-      <table className = 'table'>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table> 
+      <React.Fragment>
+        <p>Showing {this.state.movies.length} movies in database.</p>
+        <table className = 'table'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.movies.map(movie => ( 
+              <tr key = {movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button onClick={() => this.handleDelete(movie)} className='btn btn-danger btn-sm'>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table> 
+      </React.Fragment>
     );
   }
 }
